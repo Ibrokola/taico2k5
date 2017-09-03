aa71d84e4331f330b5d675c0604a1b4273f95bbc
ibrobabs <babskolawole@gmail.com>
2017-09-03 03:52:36 -0600

    update

--- topic/models.py
+++ topic/models.py
@@ -7,6 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import get_object_or_404
 
 from django.utils import timezone
+from category.models import Category 
 
 
 
