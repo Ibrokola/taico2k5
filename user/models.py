458ed9295c556582458fda79c52c4e7615b6ee81
ibrobabs <babskolawole@gmail.com>
2017-09-05 13:41:13 -0600

    update

--- user/models.py
+++ user/models.py
@@ -10,6 +10,9 @@ from django.conf import settings
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_("profile"), related_name="users_profile")
     # slug = AutoSlugField(populate_from="user.username", db_index=False, blank=True)
+    grad_year = models.PositiveIntegerField(default=0)
+    contact_no = models.PositiveIntegerField(default=0)
+    bio = models.TextField(null=True, blank=True)
     location = models.CharField(_("location"), max_length=75, blank=True)
     last_seen = models.DateTimeField(_("last seen"), auto_now=True)
     last_ip = models.GenericIPAddressField(_("last ip"), blank=True, null=True)
@@ -18,11 +21,25 @@ class UserProfile(models.Model):
     comment_count = models.PositiveIntegerField(_("comment count"), default=0)
     last_post_hash = models.CharField(_("last pos hash"), max_length=32, blank=True)
     last_post_on = models.DateTimeField(_("last post on"), null=True, blank=True)
+    website_link = models.CharField(max_length=320,
+                                    null=True,
+                                    blank=True,
+                                    verbose_name='Personal website url')
+    facebook_link = models.CharField(max_length=320,
+                                    null=True,
+                                    blank=True,
+                                    verbose_name='Facebook profile url')
+    twitter_handle = models.CharField(max_length=320,
+                                    null=True,
+                                    blank=True,
+                                    verbose_name='Twitter handle')
 
     class Meta:
         verbose_name = _("forum profile")
         verbose_name_plural = _("forum profiles")
 
+    def __str__(self):
+        return str(self.user.username)
 
     def save(self, *args, **kwargs):
         super(UserProfile, self).save(*args, **kwargs)
