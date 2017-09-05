093e93ff23d83ea0c3cfa8ddf3b6e771f7def0ba
ibrobabs <babskolawole@gmail.com>
2017-09-04 19:43:06 -0600

    update - views started

--- category/views.py
+++ category/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
+from django.http import HttpResponsePermanentRedirect
 
-# Create your views here.
+from django.views.generic import ListView
+
+
+from djconfig import config 
+
+# from core.utils.paginator import yt_paginate
+from topic.models import Topic
+from .models import Category
+
+
+
+
+class IndexView(ListView):
+    template_name = 'category/index.html'
+    context_object_name = "catgeories"
+    queryset = Category.objects.visible().parents()
+
+
+def detail(request, pk, slug):
+    category = get_object_or_404(Category.objects.visible(), pk=pk)
+
+    if category.slug != slug:
+        return HttpResponsePermanentRedirect(category.get_absolute_url())
+
+    subcategories = Category.objects.visible().children(parent=category)
+
+    
--- thomas/urls.py
+++ thomas/urls.py
@@ -6,7 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'^$', homepage, name='home'),
 	url(r'^accounts/', include('allauth.urls')),
+	url(r'^category/', include('category.urls', namespace='category')),
+	url(r'^topic/', include('topic.urls', namespace='topic'))
 ]
 
 
